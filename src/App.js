2b9b9322825da1052fbcd720f96513575cdcfac6
Daniel Ale <danielale9291@gmail.com>
2021-05-08 06:59:05 +0100

    implemtent dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-import { getSubdomain } from 'helpers/get-tenant';
+import { getSubdomain, DOMAIN } from 'helpers/get-tenant';
+import { useEffect } from 'react';
+
+const setDefaultStorage = () => {
+  const data = {
+      accessToken: "98765tfdfgyu765tgffgvcderf45678jjkk878",
+      id: "98765"
+  }
+  localStorage.setItem('token', JSON.stringify(data));
+}
+
+const setStorageData = () => {
+  const url = window.location.href;
+  let query = url.split('?token=')[1]
+  if (query){
+    query = query.split("%22").join('"')
+    localStorage.setItem('token', query)
+    window.location.href = url.split('?token=')[0]
+  }
+}
 
 function App() {
   const domain = getSubdomain();
 
+  useEffect(() => {
+    setStorageData();
+  }, [])
+
+  const handleLogin = () => {
+    setDefaultStorage();
+    const sub = '45323432';
+    const token = localStorage.getItem('token');
+    window.location.href = `http://${sub}.${DOMAIN}/?token=${token}`;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Domain is running on {`${domain}.leadwayhealth.com`}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code Away
-        </a>
-      </header>
+      <p>Domain is running on {`${domain}.${DOMAIN}`}</p>
+      <button type="button" onClick={handleLogin}>Login</button>
     </div>
   );
 }
